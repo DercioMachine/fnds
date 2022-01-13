1b06743023bf8abf41a215f394ae63630f620bab
J. Machava <machava1631>
2022-01-13 02:03:23 +0200

    Estrutura de projecto 22

--- src/main/java/mz/fipag/grm/controller/HomeController.java
+++ src/main/java/mz/fipag/grm/controller/HomeController.java
@@ -6,5 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("/registar/reclamacao")
+    public String registarReclamacao(){
+
+        return "reclamacoes/registarreclamacao";
+    }
+    @GetMapping("/reclamacao/fase2")
+    public String registarReclamacaoFase2(){
+
+        return "reclamacoes/registarreclamacaof2";
+    }
+
+    @GetMapping("/listar/reclamacao")
+    public String listarReclmacao(){
+
+        return "reclamacoes/listarreclamacao";
+    }
 
 }
