a9c2f0b87dca318bef6839d8b6a9c1132a5f27e8
Sifisso <sifissom@gmail.com>
2022-02-10 12:01:45 +0200

    s

--- src/main/java/mz/fipag/grm/GrmApplication.java
+++ src/main/java/mz/fipag/grm/GrmApplication.java
@@ -2,10 +2,12 @@ package mz.fipag.grm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
-public class GrmApplication {
+public class GrmApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 
@@ -14,11 +16,14 @@ public class GrmApplication {
 		System.out.println("Compilado com Sucesso ...");
 	}
 
-	/*
+	
 		@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(GrmApplication.class);
     }
+    
+		/*
+    
 	 * @Bean public FilterRegistrationBean<Filter> disableSpringBootErrorFilter
 	 * (ErrorPageFilter filter){ FilterRegistrationBean filterRegistrationBean = new
 	 * FilterRegistrationBean(); filterRegistrationBean.setFilter(filter);
